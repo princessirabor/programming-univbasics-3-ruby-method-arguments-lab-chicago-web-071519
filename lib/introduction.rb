7481cc054b011a35fee73f14852189359a04306d
Princess Irabor <irabor.princess1@gmail.com>
2019-06-25 20:04:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,5 @@ def introduction_with_language(name,language)
 end
 def introduction_with_language_optional(name,language)
   language = language == nil?"Ruby":language
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
